31ceccd596a8e406c6ab0673ef6339226b58aba1
unknown <sachprab543@gmail.com>
2024-01-31 18:32:26 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function App()
+{
+  return(
+    <div>
+      <h1>Welcome to our television application</h1>
+      <p>Content goes here</p>
+    </div>
+  )
+}
